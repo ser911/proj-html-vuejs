a73b148284b9b2fe22cecc3cc69a971fc66093a2
Sergio <sergio.marretta@libero.it>
2021-04-21 17:51:05 +0200

    vuejs done

--- myscript.js
+++ myscript.js
@@ -22,35 +22,27 @@ new Vue ({
 el: "#app2",
 data: {
 
-social_icons1: [
-  {social:"ig",
+social_icons: [
+  {social:"fb",
     icon: "facebook-f-brands.svg"
+},
+
+{social:"tw",
+  icon: "twitter-brands.svg"
+},
+
+{social:"tw",
+  icon: "instagram-square-brands.svg"
+},
+
+{social:"pt",
+icon: "pinterest-p-brands.svg"
 },
 
 
 
 ],
-social_icons2: [
 
-  {social:"fb"},
-
-
-
-
-],
-social_icons3: [
-  {social:"tw"},
-
-
-
-
-],
-social_icons4: [
-{social:"pt"},
-
-
-
-],
 }
 
 }
