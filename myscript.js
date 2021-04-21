7ca55c61cd483e5501cf9d16d8df5db58139586f
Sergio <sergio.marretta@libero.it>
2021-04-21 16:47:58 +0200

    wip

---
+++ myscript.js
@@ -0,0 +1,57 @@
+
+var app = new Vue ({
+el: "#app",
+data: {
+
+
+navBar: [
+{element:"Home"},
+{element:"Pages"},
+{element:"Portfolio"},
+{element:"Blog"},
+{element:"Shop"},
+{element:"Elements"}
+],
+
+},
+
+
+}
+);
+new Vue ({
+el: "#app2",
+data: {
+
+social_icons1: [
+  {social:"ig",
+    icon: "facebook-f-brands.svg"
+},
+
+
+
+],
+social_icons2: [
+
+  {social:"fb"},
+
+
+
+
+],
+social_icons3: [
+  {social:"tw"},
+
+
+
+
+],
+social_icons4: [
+{social:"pt"},
+
+
+
+],
+}
+
+}
+);
